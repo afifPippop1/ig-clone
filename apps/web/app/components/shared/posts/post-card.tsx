aa5893f150823cab09a7e79f62aadfb8f8412d11
afifPippop1 <afifpippop@gmail.com>
2025-07-08 22:24:55 +0700

    feat: 🎸 post card link and like button

--- apps/web/app/components/shared/posts/post-card.tsx
+++ apps/web/app/components/shared/posts/post-card.tsx
@@ -1,18 +1,22 @@
 import { FeedOutput } from '@ig-clone/database';
 import { Avatar, AvatarFallback, AvatarImage } from '~/components/ui/avatar';
 import { FollowButton } from '../profile/follow-button';
+import { Link } from '@remix-run/react';
+import { LikeButton } from './like-button';
 
 export function PostCard({ feed }: { feed: FeedOutput }) {
   return (
     <div className="w-full flex flex-col items-stretch gap-4 border-b-2 border-b-secondary pb-16">
       <div className="flex justify-between">
-        <div className="flex gap-4">
-          <Avatar>
-            <AvatarImage src={feed.user.profile.photoProfilePath || ''} />
-            <AvatarFallback>{feed.user.username[0]}</AvatarFallback>
-          </Avatar>
-          <p className="font-bold">{feed.user.username}</p>
-        </div>
+        <Link to={`/${feed.user.username}`}>
+          <div className="flex gap-4">
+            <Avatar>
+              <AvatarImage src={feed.user.profile.photoProfilePath || ''} />
+              <AvatarFallback>{feed.user.username[0]}</AvatarFallback>
+            </Avatar>
+            <p className="font-bold">{feed.user.username}</p>
+          </div>
+        </Link>
         <FollowButton user={feed.user} />
       </div>
       <div className="outline-2 outline-secondary rounded-lg overflow-hidden">
@@ -22,6 +26,7 @@ export function PostCard({ feed }: { feed: FeedOutput }) {
           className="h-full w-full object-contain"
         />
       </div>
+      <LikeButton postId={feed.id} />
     </div>
   );
 }
